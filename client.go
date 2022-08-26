603670beed909131769031c74fa3098f177c243e
李光春 <gc@dtapp.net>
2022-08-26 16:07:58 +0800

    - update log

--- client.go
+++ client.go
@@ -51,7 +51,7 @@ func NewClient(config *ClientConfig) (*Client, error) {
 	c.requestClient = gorequest.NewHttp()
 
 	gormClient := config.GormClientFun()
-	if gormClient.Db != nil {
+	if gormClient != nil && gormClient.Db != nil {
 		c.log.logGormClient, err = golog.NewApiGormClient(func() (*dorm.GormClient, string) {
 			return gormClient, logTable
 		}, config.Debug)
@@ -59,11 +59,11 @@ func NewClient(config *ClientConfig) (*Client, error) {
 			return nil, err
 		}
 		c.log.gorm = true
+		c.log.gormClient = gormClient
 	}
-	c.log.gormClient = gormClient
 
 	mongoClient, databaseName := config.MongoClientFun()
-	if mongoClient.Db != nil {
+	if mongoClient != nil && mongoClient.Db != nil {
 		c.log.logMongoClient, err = golog.NewApiMongoClient(func() (*dorm.MongoClient, string, string) {
 			return mongoClient, databaseName, logTable
 		}, config.Debug)
@@ -71,8 +71,8 @@ func NewClient(config *ClientConfig) (*Client, error) {
 			return nil, err
 		}
 		c.log.mongo = true
+		c.log.mongoClient = mongoClient
 	}
-	c.log.mongoClient = mongoClient
 
 	return c, nil
 }
--- const.go
+++ const.go
@@ -8,4 +8,4 @@ const (
 	logTable = "meituan"
 )
 
-const Version = "1.0.12"
+const Version = "1.0.13"
