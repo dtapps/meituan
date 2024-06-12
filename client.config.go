06c09dcac4d59d7e341346e5813ee025c79b93e3
dtapps <li.gc@icloud.com>
2024-06-12 16:31:48 +0800

    - update

--- client.config.go
+++ client.config.go
@@ -10,15 +10,23 @@ func (c *Client) ConfigClient(config *ClientConfig) {
 	c.config.appKey = config.AppKey
 }
 
+func (c *Client) SetSecret(secret string) *Client {
+	c.config.secret = secret
+	return c
+}
+
+func (c *Client) SetAppKey(appKey string) *Client {
+	c.config.appKey = appKey
+	return c
+}
+
 // SetClientIP 配置
-func (c *Client) SetClientIP(clientIP string) {
-	if clientIP == "" {
-		return
-	}
+func (c *Client) SetClientIP(clientIP string) *Client {
 	c.config.clientIP = clientIP
 	if c.httpClient != nil {
 		c.httpClient.SetClientIP(clientIP)
 	}
+	return c
 }
 
 // SetLogFun 设置日志记录函数
